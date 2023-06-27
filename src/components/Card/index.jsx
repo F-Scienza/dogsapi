ac51b722f121bf14b75f36f52bc05b54aa049f7d
F-Scienza <cufa1905@gmail.com>
2023-06-27 17:06:04 -0300

    manejo de favoritos

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -8,16 +8,22 @@ function Card({ dog }) {
 	const context = useContext(DogsContext); // traemos el contexto
 	const { favDogs, setFavDogs } = context;
 
+	const updatedFavDogs = favDogs.filter((favDog) => favDog !== dog)
 
-	const handleAddToFav = () => {
+	const handleFavs = () => {
 		// agregamos al contexto
-		setFavDogs([...favDogs, dog])
+		favDogs.includes(dog)
+			? setFavDogs(updatedFavDogs)
+			: setFavDogs([...favDogs, dog]);
 	};
+
+	// clase por si el perrito ya está en favoritos 
+	const faved = favDogs.includes(dog) ? 'faved' : 'fav-btn';
 
 	return (
 		<div className="card-dogs">
 			<img className="card-img" key={dog.id} src={dog.url} alt="dog" />
-			<button onClick={handleAddToFav} className="fav-btn">
+			<button onClick={handleFavs} className={faved}>
 				<img className="fav-btn-img" src={like} alt="like" />
 			</button>
 		</div>
